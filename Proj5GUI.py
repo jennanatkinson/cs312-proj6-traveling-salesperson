7b105d91250757cf2f8cbe3bcdd4bfe895f00572
Jenna Atkinson <>
2022-12-05 14:12:21 -0700

    Fix spacing

--- Proj5GUI.py
+++ Proj5GUI.py
@@ -110,9 +110,9 @@ class PointLineView( QWidget ):
 		h = self.height()
 		w2h_desired_ratio = (xr[1]-xr[0])/(yr[1]-yr[0])
 		if w / h < w2h_desired_ratio:
-			 scale = w / (xr[1]-xr[0])
+			scale = w / (xr[1]-xr[0])
 		else:
-			 scale = h / (yr[1]-yr[0])
+			scale = h / (yr[1]-yr[0])
 
 		tform = QTransform()
 		tform.translate(self.width()/2.0,self.height()/2.0)
