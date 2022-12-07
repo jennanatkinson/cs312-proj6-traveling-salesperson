d29ab86ece9c059a40f0537b722d44c5e771c3b3
Jenna Atkinson <>
2022-12-06 23:23:59 -0700

    Add tests

--- test.py
+++ test.py
@@ -30,3 +30,55 @@ def test_should_solve_easy_small_greedy():
   assert(results['time'] < 60)
   assert(results['count'] == 3)
   assert(results['solution'] != None)
+
+def test_should_solve_easy_three_cheapest_insertion():
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+  app = QApplication(sys.argv)
+  w = Proj5GUI()
+  w.generateNetwork(size='3', seed='20', diff='Easy')
+  w.solver.setupWithScenario(w._scenario)
+  max_time = float(60)
+
+  results = w.solver.fancy(max_time)
+
+  assert(results['cost'] == 4159)
+  assert(results['time'] < 60)
+  assert(results['count'] == 3) #
+  assert(results['solution'] != None)
+  assert(len(results['solution'].route) == 3)
+
+def test_should_solve_easy_ten_cheapest_insertion():
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+  app = QApplication(sys.argv)
+  w = Proj5GUI()
+  w.generateNetwork(size='10', seed='431', diff='Easy')
+  w.solver.setupWithScenario(w._scenario)
+  max_time = float(60)
+
+  results = w.solver.fancy(max_time)
+
+  assert(results['cost'] <= 7242) # Optimal solution that greedy found
+  assert(results['time'] < 60)
+  assert(results['count'] <= 10) # Find at most 10 solutions
+  assert(results['solution'] != None)
+  assert(len(results['solution'].route) == 10)
+
+
+def test_should_solve_normal_ten_cheapest_insertion():
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+  app = QApplication(sys.argv)
+  w = Proj5GUI()
+  w.generateNetwork(size='10', seed='850', diff='Normal')
+  w.solver.setupWithScenario(w._scenario)
+  max_time = float(60)
+
+  results = w.solver.fancy(max_time)
+
+  assert(results['cost'] <= 8247) # Optimal solution that greedy found
+  assert(results['time'] < 60)
+  assert(results['count'] <= 10) # Find at most 10 solutions
+  assert(results['solution'] != None)
+  assert(len(results['solution'].route) == 10)
